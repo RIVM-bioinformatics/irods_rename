890e76e276c23de625a6c3d206f37ec91d129b85
boasvdp <boasvdp@gmail.com>
2024-04-05 15:36:18 +0200

    feat: improve checks

--- rename.py
+++ rename.py
@@ -58,17 +58,166 @@ def check_which_files_need_rename(list_of_files, rename_sheet):
     logging.info(f"Converting list_of_files to Path objects")
     list_of_files = [Path(f) for f in list_of_files]
     for line in lines:
-        original_filename, new_filename = line.strip().split(",")
-        logging.info(f"{original_filename} -> {new_filename}")
+        try:
+            original_filename, new_filename = line.strip().split(",")
+        except ValueError:
+            if line.strip() == "":
+                raise ValueError("Empty line in rename sheet")
+            elif "," not in line:
+                raise ValueError(
+                    f"Line {line} in rename sheet does not contain a comma"
+                )
+            else:
+                raise ValueError(
+                    f"Line {line} in rename sheet is not in the correct format"
+                )
+        original_filename = Path(original_filename)
+        new_filename = Path(new_filename)
+        logging.info(f"Rename instruction: {original_filename} -> {new_filename}")
         if original_filename in list_of_files:
-            logging.info(f"File {original_filename} also found in directory")
-            rename_dict[original_filename] = new_filename
+            logging.info(
+                f"File {original_filename} also found in directory, including in rename decision dictionary"
+            )
+            if original_filename in rename_dict:
+                raise ValueError(
+                    f"File {original_filename} found in rename sheet {rename_sheet} more than once"
+                )
+            else:
+                rename_dict[original_filename] = new_filename
         else:
             logging.warning(
                 f"File {original_filename} found in rename sheet {rename_sheet} but not in directory"
             )
     logging.info(f"Finished checking which files need to be renamed")
     return rename_dict
+
+
+def check_uniqueness_filenames(label, unique_values, rename_decision_per_file):
+    """
+    Check whether the new filenames are unique.
+
+    Params
+    ------
+    label : str
+        A label for raising a possible error.
+    unique_values : set
+        A set of unique values.
+    rename_decision_per_file : dict
+        A dictionary with the original filename as the key and the new filename as the value.
+
+    Raises
+    ------
+    ValueError
+        If the filenames are not unique.
+
+    """
+    logging.info(f"Checking uniqueness of {label}")
+    if len(unique_values) != len(rename_decision_per_file):
+        logging.error(f"{label} are not unique")
+        raise ValueError(f"{label} are not unique")
+
+
+def check_possible_filename_switch(rename_decision_per_file):
+    """
+    Check if there are any filename switches in the rename decision.
+
+    Params
+    ------
+    rename_decision_per_file : dict
+        A dictionary with the original filename as the key and the new filename as the value.
+
+    Raises
+    ------
+    ValueError
+        If there are filename switches in the rename decision.
+
+    """
+    # filter out entries where the new filename is the same as the original filename
+    rename_dict_filtered = {
+        original_filename: new_filename
+        for original_filename, new_filename in rename_decision_per_file.items()
+        if original_filename != new_filename
+    }
+    # check if any new filename is the same as an original filename of another entry
+    new_filenames_filtered = set(rename_dict_filtered.values())
+    original_filenames_filtered = set(rename_dict_filtered.keys())
+    if len(new_filenames_filtered.intersection(original_filenames_filtered)) > 0:
+        logging.error(
+            f"Some entries have new filenames that are the same as original filenames for other entries"
+        )
+        raise ValueError(
+            "Some entries have new filenames that are the same as original filenames for other entries"
+        )
+
+
+def check_validity_of_rename_decision(rename_decision_per_file):
+    """
+    Perform some checks on the validiy of rename decisions.
+
+    Params
+    ------
+    rename_decision_per_file : dict
+        A dictionary with the original filename as the key and the new filename as the value.
+
+    """
+    logging.info(f"Checking validity of rename decision")
+    check_uniqueness_filenames(
+        "new filenames",
+        set(rename_decision_per_file.values()),
+        rename_decision_per_file,
+    )
+    check_uniqueness_filenames(
+        "old filenames", set(rename_decision_per_file.keys()), rename_decision_per_file
+    )
+    check_possible_filename_switch(rename_decision_per_file)
+    logging.info(f"Rename decision is valid")
+
+
+def create_output_directory(input_directory, output_directory):
+    """
+    Create an output directory and copy the contents of the input directory to it.
+
+    Params
+    ------
+    input_directory : Path
+        The input directory.
+    output_directory : Path
+        The output directory.
+
+    Returns
+    -------
+    None
+
+    """
+    if output_directory.exists():
+        logging.info(
+            f"Output directory {output_directory} already exists, removing before copying"
+        )
+        shutil.rmtree(output_directory)
+    shutil.copytree(input_directory, output_directory)
+
+
+def rename_files(output_directory, rename_decision_per_file):
+    """
+    Rename files in the output directory.
+
+    Params
+    ------
+    output_directory : Path
+        The output directory.
+    rename_decision_per_file : dict
+        A dictionary with the original filename as the key and the new filename as the value.
+
+    Returns
+    -------
+    None
+
+    """
+    for old_filename, new_filename in rename_decision_per_file.items():
+        old_filepath = output_directory / old_filename
+        new_filepath = output_directory / new_filename
+        logging.info(f"Renaming {old_filepath} to {new_filepath}")
+        old_filepath.rename(new_filepath)
 
 
 def main(args):
